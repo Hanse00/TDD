9fd0e762edc2adb6a8979ded58a7b415b99e55de
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2025-10-12 10:19:40 -0700

    Adds multiplication support

--- WyCash/app/src/test/java/org/example/AppTest.java
+++ WyCash/app/src/test/java/org/example/AppTest.java
@@ -8,4 +8,10 @@ class AppTest {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
+
+    @Test void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
 }
