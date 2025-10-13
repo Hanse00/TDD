e00fa9f1f085f5b005d25e6f763e88298a8ba93f
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2025-10-12 20:53:51 -0700

    Ensures different currencies aren't equal

--- WyCash/app/src/main/java/org/mallegolhansen/philip/WyCash/Money.java
+++ WyCash/app/src/main/java/org/mallegolhansen/philip/WyCash/Money.java
@@ -5,6 +5,6 @@ public class Money {
 
     public boolean equals(Object object) {
         Money other = (Money) object;
-        return amount == other.amount;
+        return amount == other.amount && getClass() == other.getClass();
     }
 }
