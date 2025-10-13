9e4a9240814a17a8b9e81fda586a0d4053d4c8a4
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2025-10-12 20:51:43 -0700

    Removes redundant code in Franc

--- WyCash/app/src/main/java/org/mallegolhansen/philip/WyCash/Franc.java
+++ WyCash/app/src/main/java/org/mallegolhansen/philip/WyCash/Franc.java
@@ -1,19 +1,12 @@
 package org.mallegolhansen.philip.WyCash;
 
-public class Franc {
-    private int amount;
-
+public class Franc extends Money {
     Franc(int amount) {
         this.amount = amount;
     }
 
     Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc other = (Franc) object;
-        return amount == ((Franc) object).amount;
     }
 }
 
