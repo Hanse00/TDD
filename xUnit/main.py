fcd11c4c4d7d3b5980f03549aa99283d549bf2b0
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2025-10-14 15:14:50 -0700

    Adds setUp support

--- xUnit/main.py
+++ xUnit/main.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -12,21 +16,31 @@ class WasRun(TestCase):
         self.wasRun = False
         TestCase.__init__(self, name)
 
+    def setUp(self):
+        self.wasRun = False
+        self.wasSetUp = True
+
     def testMethod(self):
         self.wasRun = True
 
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
+
+    def testSetUp(self):
+        self.test.run()
+        assert self.test.wasSetUp
 
 
 
 def main():
     TestCaseTest("testRunning").run()
+    TestCaseTest("testSetUp").run()
 
 
 if __name__ == "__main__":
