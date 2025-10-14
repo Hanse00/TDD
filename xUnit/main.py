01e259c7b73f16bfa918d50b6570dd46632a4fb0
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2025-10-14 15:05:03 -0700

    Creates basic unit test driver

--- xUnit/main.py
+++ xUnit/main.py
@@ -1,5 +1,32 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = False
+        TestCase.__init__(self, name)
+
+    def testMethod(self):
+        self.wasRun = True
+
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert not test.wasRun
+        test.run()
+        assert test.wasRun
+
+
+
 def main():
-    print("Hello from xunit!")
+    TestCaseTest("testRunning").run()
 
 
 if __name__ == "__main__":
