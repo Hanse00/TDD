4a152af1bfe74b8f25db5de14095f8617120cbf1
Philip Mallegol-Hansen <philip@mallegolhansen.com>
2025-10-14 15:23:40 -0700

    Adds teardown

--- xUnit/main.py
+++ xUnit/main.py
@@ -5,34 +5,35 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = False
         TestCase.__init__(self, name)
 
     def setUp(self):
-        self.wasRun = False
         self.log = "setUp "
 
     def testMethod(self):
-        self.wasRun = True
         self.log += "testMethod "
+
+    def tearDown(self):
+        self.log += "tearDown "
 
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        self.test.run()
-        assert "setUp testMethod " == self.test.log
+        test.run()
+        assert "setUp testMethod tearDown " == test.log
 
 
 
